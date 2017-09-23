30972de7c5ae25b883dadedfee1de3a4a718dca6
Jennifer Wang <Jwang28@gmail.com>
2017-09-22 21:58:07 -0400

    Add base algorithm still need to adjust for printing and changing leaf value

--- Solution3.java
+++ Solution3.java
@@ -11,75 +11,57 @@ public class Solution3 extends Twothree {
     	int num_input = Integer.parseInt(input);
     	Solution3 tree = new Solution3();
     	TwoThreeTree root = new TwoThreeTree();
+      int query;
 
 
     	for (int i = 0; i < num_input; i++){
     		input = reader.readLine();
     		String [] words = input.split(" ");
         //if first is 1, insert
-        if ()
-
-        //if first is 2, range update
-
-        //if first is 3, return entrance fee from planet
-
-        //add to database
-    		tree.insert(words[0], Integer.parseInt(words[1]), root);
-    	}
-
-      input = reader.readLine();
-      num_input = Integer.parseInt(input);
-      for (int i = 0; i < num_input; i++){
-        input = reader.readLine();
-        String [] words = input.split(" ");
-        rangeFind(root, words[0], words[1]);
-      }
-    }
-
-    static void rangeFind(TwoThreeTree tree, String start, String end){
-      int h = tree.height;
-
-      if (h == -1){
-        return;
-      }
-      else{
-        if (start.compareTo(end) > 0 ){
-          String tmp = start;
-          start = end;
-          end = tmp;
+        query = Integer.parseInt(words[0]);
+        if (query == 1){
+          //insert
+          tree.insert(words[1], Integer.parseInt(words[2]), root);
         }
-        doRangeFind(tree.root, start, end, h);
-      }
-    }
-
-    static void doRangeFind (Node p, String start, String end, int h){
-      if (h == 0) {
-         // we're at the leaf level
-
-         LeafNode leaf = (LeafNode) p;
-         int cmp = end.compareTo(leaf.guide);
-
-         if (cmp >= 0) {
-
-            System.out.println(leaf.guide + ' ' + leaf.value);
-         }
-      }
-      else {
-         InternalNode q = (InternalNode) p;
-         int pos;
-
-         if (start.compareTo(q.child0.guide) <= 0) {
-            
-            doRangeFind(q.child0, start, end, h-1);
-         }
-         if (start.compareTo(q.child1.guide) <= 0) {
+        else if(query == 2){
           
-            doRangeFind(q.child1, start, end, h-1);
-         }
-         if (q.child2 != null && start.compareTo(q.child2.guide) <= 0) {
-       
-            doRangeFind(q.child2, start, end, h-1);
-         }
+          tree.updateRange (root.root.guide, root.root, words[1], words[2], root.height, words[3]);
+          System.out.println(root.root.guide);
+        }
+        else{
+          //return entrance fee
+          System.out.println("query is 3");
+        }
+    	}
+    }
+
+  static void updateRange (String low, Node n, String left, String right, int height, String value){
+    System.out.println("value is: " + value);
+
+    if (height == 0){
+      if (n.guide.compareTo(left) >= 0 && n.guide.compareTo(right) <= 0){
+        System.out.println("guide is: " + n.guide);
+        n.guide+= value;
+      }
+      return;
+    }
+    //fully in range
+    if (left.compareTo(low) <= 0 && right.compareTo(n.guide) >= 0){
+      System.out.println("original n.guide: " + n.guide);
+      n.guide+=value;
+      System.out.println("part 2: " + n.guide);
+    }
+
+    //intersects
+    else if(right.compareTo(low) > 0 && left.compareTo(n.guide) <= 0){
+      InternalNode q = (InternalNode) n;
+      updateRange(low, q.child0, left, right, height - 1, value);
+      updateRange(q.child0.guide, q.child1, left, right, height - 1, value);
+      if (q.child2 != null){
+        updateRange(q.child1.guide, q.child2, left, right, height - 1, value);
       }
     }
+
+    //do nothing if not in range
+  }
 }
